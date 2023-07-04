68aae0d2fcb09e1621576e4c148b485f457f62b0
Diana Clabel <dianaclabel08@gmail.com>
2023-07-04 08:31:18 -0500

    Se grega funcion validate

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const { fnIsAbsolute , fnConvertToAbsolute, verifyRoute, veriFyIsFileOrDirectory, readDirectory , readFile , createPathFile, readLinks } = require("./main.js");
+const {
+  fnIsAbsolute,
+  fnConvertToAbsolute,
+  verifyRoute,
+  veriFyIsFileOrDirectory,
+  readDirectory,
+  readFile,
+  createPathFile,
+  readLinks,
+} = require("./main.js");
 
-function mdLinks(path){
-    // Manejo de ruta relativa
+function mdLinks(path) {
+  // Manejo de ruta relativa
   const pathIsAbsolute = fnIsAbsolute(path);
-  
+
   if (!pathIsAbsolute) {
     const absoluteVersion = fnConvertToAbsolute(path);
-    console.log("se covierte a ruta absoluta: "+ absoluteVersion)
+    console.log("se covierte a ruta absoluta: " + absoluteVersion);
     return mdLinks(absoluteVersion);
   }
 
@@ -23,55 +32,58 @@ function mdLinks(path){
   console.log("La ruta es de tipo: " + pathType);
 
   readFileOrDirectory(path, pathType)
-    .then(links => {
+    .then((links) => {
       console.log(links);
     })
-    .catch(error => {
-      console.log('Error al obtener los archivos:', error);
-    });;
-
+    .catch((error) => {
+      console.log("Error al obtener los archivos:", error);
+    });
 }
 
-function readFileOrDirectory(path, type){
-    if(type === "directory"){
-        
-         return readDirectory(path)
-          .then(fileNames => {
-              // array de nombres de archivos
-              console.log("Nombres de los archivos de directorio: ", fileNames);
+function readFileOrDirectory(path, pathtype) {
+  if (pathtype === "directory") {
+    return readDirectory(path).then((fileNames) => {
+      // array de nombres de archivos
+      console.log("Nombres de los archivos de directorio: ", fileNames);
 
-              const promisesFilesLinks = fileNames
-                .filter(fileName => fileName.endsWith(".md"))
-                .map(fileName => {
-                  let pathFile = createPathFile(path, fileName);
-                  console.log("Ruta de archivo creada:", pathFile);
-                  //retorna los links del archivo
-                  return readFile(pathFile).then(data => readLinks(data))
-                }) 
+      const promisesFilesLinks = fileNames
+        .filter((fileName) => fileName.endsWith(".md"))
+        .map((fileName) => {
+          let pathFile = createPathFile(path, fileName);
+          console.log("Ruta de archivo creada:", pathFile);
+          //retorna la data - el contenido de archivo en string
+          return readFile(pathFile).then((data) => {
+            // Array de links (text, href)
+            const links = readLinks(data);
+            //retornara text , href y filename
+            return links.map((link) => {
+              return { ...link, file: fileName };
+            });
+          });
+        });
 
-                //Se retorna todas las promesas
-              return Promise.all(promisesFilesLinks);
+      //Se retorna todas las promesas
+      return Promise.all(promisesFilesLinks);
+    });
+  } else if (pathtype === "file") {
+    if (!path.endsWith(".md")) {
+      console.error("El archivo no es md");
+      return;
+    }
 
-            })
-        
-        
-    }else if (type === "file") {
-    
-        if (!path.endsWith(".md")) {
-          console.error("El archivo no es md");
-          return;
-        }
-    
-        return readFile(path)
-        .then(data => readLinks(data))
-        .catch(error => console.log('Este archivo no se puede leer',error))
-    } 
-          
+    return readFile(path)
+      .then((data) => {
+        // Array de links
+        const links = readLinks(data);
+        //retornara text , href y filename
+        return links.map((link) => {
+          return { ...link, file: path.split("/").at(-1) };
+        });
+      })
+      .catch((error) => console.log("Este archivo no se puede leer", error));
+  }
 }
 
-
-mdLinks("C:/Users/diana/Documents/Projects/Laboratoria/md-links/files-md");
-
-
-
-
+mdLinks(
+  "C:/Users/diana/Documents/Projects/Laboratoria/md-links/test/files-md/"
+);
--- main.js
+++ main.js
@@ -1,118 +1,133 @@
 const path = require("path");
 const fs = require("fs");
-const { promisify } = require('util');
-
+const { promisify } = require("util");
 
 // funcion para verificar si es una ruta absoluta
 // retorna booleano
 
 function fnIsAbsolute(route) {
   try {
-    return  path.isAbsolute(route);
-    
+    return path.isAbsolute(route);
   } catch (error) {
-    console.log('Error: La ruta no es absoliuta ', error); 
+    console.log("Error: La ruta no es absoluta ", error);
   }
 }
 
 // convertira una ruta relativa a absoluta
 // retorna  una ruta relativa en string
 
-function fnConvertToAbsolute(route){
-    
-    return path.resolve(route);  
+function fnConvertToAbsolute(route) {
+  return path.resolve(route);
 }
 
 // verificación de existencia
 // retorna booleano
 
-function verifyRoute(route){
+function verifyRoute(route) {
   try {
     fs.accessSync(route);
-   return true;
+    return true;
   } catch (error) {
-    return false
+    return false;
   }
 }
 
 // Verificar si es un archivo o directorio
 
-function veriFyIsFileOrDirectory (route){
+function veriFyIsFileOrDirectory(route) {
   try {
     const inspectRoute = path.resolve(route);
     const stats = fs.statSync(inspectRoute); // Obtener información sobre el archivo o directorio especificado (inspectRoute)
-     // devuelve un objeto stats, contiene detalles sobre el archivo o directorio.
+    // devuelve un objeto stats, contiene detalles sobre el archivo o directorio.
     if (stats.isFile()) {
-        return 'file';
-    } else if(stats.isDirectory()){
-        return 'directory';
-    }else{
-        return 'Desconocido';
+      return "file";
+    } else if (stats.isDirectory()) {
+      return "directory";
+    } else {
+      return "Desconocido";
     }
   } catch (error) {
-            console.log('Error: Archivo o directorio roto ', error); 
+    console.log("Error: Archivo o directorio roto ", error);
   }
 }
 
 //Mostrar lista de archivos de un directorio
-// retorna el nombre de los archivos 
+// retorna el nombre de los archivos
 
-function readDirectory (directoryRoute){
+function readDirectory(directoryRoute) {
   // promisify es una funcion de modulo util, convierte en una funcion callback a funcion que devuelve una promesa
   const readdir = promisify(fs.readdir);
   return readdir(directoryRoute);
 }
 
-/**
- * Función para leer los enlaces de un Markdown string
- * @param {string} data 
- */
-function readLinks(data){
+function readLinks(data) {
   // [Acerca de Node.js - Documentación oficial](https://nodejs.org/es/about/)
 
   const iterable = data.matchAll(/\[([^\]]+)\]\(([^\)]+)\)/g);
   // se convierte el iterable en un array
   const results = [...iterable];
 
-  const links = results.map(result => ({ text : result[1], href: result[2] }))
-  
+  const links = results.map((result) => {
+    return { text: result[1], href: result[2] };
+  });
+  // console.log(links);
+
   return links;
 }
 
-//Leer archivos 
+//Leer archivos
 // retorna el contenido - data del archivo
 function readFile(filePath) {
-   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (error, data) => {
-			if (error) return reject(error);
-			resolve(data);
-		}); 
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (error, data) => {
+      if (error) return reject(error);
+      resolve(data);
+    });
   });
-} 
+}
 
-//Este codigo va en md-liks
-// readFile("C:/Users/diana/Documents/Projects/Laboratoria/md-links/README.md")
-// .then(data => {
-//   // console.log(data) 
-//   readLinks(data);
-// })
-// .catch(error => console.log('Este archivo no se puede leer',error)); 
-
+// Este codigo va en md-liks
+// readFile(
+//   "C:/Users/diana/Documents/Projects/Laboratoria/md-links/test/files-md/prueba2.md"
+// )
+//   .then((data) => {
+//     // console.log(data)
+//     readLinks(data);
+//   })
+//   .catch((error) => console.log("Este archivo no se puede leer", error));
 
 // creacion de ruta con el nombre de archivo iterado
 // retorna la ruta del archivo
 
-function createPathFile(route,fileName){
-  return pathFile = path.join(route,fileName)
-   
-};
+function createPathFile(route, fileName) {
+  return (pathFile = path.join(route, fileName));
+}
 
 // createPathFile("C:/Users/diana/Documents/Projects/Laboratoria/md-links/files-md","/prueba2.md");
 // console.log(veriFyIsFileOrDirectory ("C:/Users/diana/Documents/Projects/Laboratoria/md-links/README.md"));
 // console.log( readDirectory("C:/Users/diana/Documents/Projects/Laboratoria/md-links"));
 
+function validate(url) {
+  return fetch(url)
+    .then((response) => {
+      const status = response.status;
+      const ok = response.ok ? "ok" : "fail";
+      return { status, ok };
+    })
+    .catch(() => {
+      return { status: -1, ok: "fail" };
+    });
+}
 
+validate("https://api.nijrex.pe/asdasd");
 
 module.exports = {
-  fnIsAbsolute , fnConvertToAbsolute, verifyRoute, veriFyIsFileOrDirectory, readDirectory , readFile, readLinks, createPathFile
+  fnIsAbsolute,
+  fnConvertToAbsolute,
+  verifyRoute,
+  veriFyIsFileOrDirectory,
+  readDirectory,
+  readFile,
+  readLinks,
+  createPathFile,
 };
